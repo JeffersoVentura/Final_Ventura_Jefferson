a28ecb06b330e4d8899d044a4a5be7997baa9f37
JefferVentu <jvruiz62@gmail.com>
2022-08-26 18:56:15 -0500

    Modelo

--- Final_Ventura_Jefferson/src/main/java/Modelo/PaisArreglo.java
+++ Final_Ventura_Jefferson/src/main/java/Modelo/PaisArreglo.java
@@ -8,8 +8,14 @@ public class PaisArreglo {
     private Pais [] pais;
     private int indice;
     
+    private Ciudad ciudades;
+    
     public PaisArreglo (int tamano){
         this.pais = new Pais[tamano];
         this.indice = 0;
+    }
+    
+    public void getCiudadades (String codPais){
+        
     }
 }
